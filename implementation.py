fe0ea5981587140cba9e5c75ff74c792f9ad19a7
Nikita Sapunov <kiton1994@gmail.com>
2017-03-05 17:34:55 +0300

    Add initial files

---
+++ implementation.py
@@ -0,0 +1,20 @@
+class BaseBin:
+
+    items = []
+
+    def search(self):
+        pass
+
+
+class IntBin(BaseBin):
+
+    def __init__(self, *args):
+
+        pass
+
+
+class StringBin(BaseBin):
+
+    def __init__(self, *args):
+
+        pass
